2e6e0456cdc38e09808a702ca1bccf3f991a5d5a
Shirley Zhou <Shirley@Shirleys-MacBook-Pro.local>
2017-09-21 19:34:06 -0400

    init commit for dribble api web app

---
+++ dribbble_req.js
@@ -0,0 +1,22 @@
+
+function api_request() {
+    $.getjson('https://api.dribbble.com/v1/shots?per_page=20&access_token=5f8acda96311be5321f29c1e9edb2223146d59c7a8072d0b4e9b7c7eddf7174c&callback=?', 
+        function(e){
+            $.each(e.data, function(i, val) {                
+                db.img[i] = { src: val.images.hidpi, 
+                    desc: val.description, 
+                    title: val.title, 
+                    author: val.user.name}
+            })
+        console.log(e);
+        })
+}
+
+$(document).ready(function(){
+    // API Call and Image Initiallization
+    api_request();
+});
+
+
+
+
