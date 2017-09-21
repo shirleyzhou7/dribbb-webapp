d225182ac43f987b8de2aab72cf9ea3640bb8062
Shirley Zhou <Shirley@Shirleys-MacBook-Pro.local>
2017-09-21 19:53:09 -0400

    fixing changesA

--- dribbble_req.js
+++ dribbble_req.js
@@ -1,6 +1,6 @@
 
 function api_request() {
-    $.getjson('https://api.dribbble.com/v1/shots?per_page=20&access_token=5f8acda96311be5321f29c1e9edb2223146d59c7a8072d0b4e9b7c7eddf7174c&callback=?', 
+    $.getjson('https://api.dribbble.com/v1/shots/3821382?access_token=5f8acda96311be5321f29c1e9edb2223146d59c7a8072d0b4e9b7c7eddf7174c', 
         function(e){
             $.each(e.data, function(i, val) {                
                 db.img[i] = { src: val.images.hidpi, 
