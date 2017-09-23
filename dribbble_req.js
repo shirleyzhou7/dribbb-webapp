e0ccd5e6a729658cf484ee2bbb17d8e354d6da53
Shirley Zhou <Shirley@Shirleys-MacBook-Pro.local>
2017-09-23 10:06:48 -0400

    finally successfully pulled data from dribbble api

--- dribbble_req.js
+++ dribbble_req.js
@@ -1,22 +1,39 @@
+// I am using Dribbble API which allows CORS. 
+// Dribbble is a platform where designers can share their original work
+// displayed as "shots" which are 300x400 snapshots of a design 
 
-function api_request() {
-    $.getjson('https://api.dribbble.com/v1/shots/3821382?access_token=5f8acda96311be5321f29c1e9edb2223146d59c7a8072d0b4e9b7c7eddf7174c', 
-        function(e){
-            $.each(e.data, function(i, val) {                
-                db.img[i] = { src: val.images.hidpi, 
-                    desc: val.description, 
-                    title: val.title, 
-                    author: val.user.name}
-            })
-        console.log(e);
-        })
-}
+// API documentation found here: http://developer.dribbble.com/v1/
 
 $(document).ready(function(){
     // API Call and Image Initiallization
-    api_request();
+    $('#local').click(api_request);
 });
 
+
+function api_request() {
+    let access_token = '5f8acda96311be5321f29c1e9edb2223146d59c7a8072d0b4e9b7c7eddf7174c'
+    $("#responseArea").html("");
+    $.getJSON('https://api.dribbble.com/v1/shots/3821382?access_token='+access_token, 
+        function(data,status){
+            if (status === "success") parseData(data);
+        });
+}
+
+
+
+function parseData(obj){
+    var displayTags;
+    for (var i =0; i < obj.tags.length; i++) {                
+        var tag = obj.tags[i];
+        displayTags += "<li>" + tag + "<\/li>";
+    };
+    var image = '<img src=' + obj.images.hidpi +'>';
+    var desc = obj.description;
+    console.log(obj);
+    $("#descrip").html(desc);
+    $("#tags").html(displayTags);
+    $("#image").html(image);
+}
 
 
 
